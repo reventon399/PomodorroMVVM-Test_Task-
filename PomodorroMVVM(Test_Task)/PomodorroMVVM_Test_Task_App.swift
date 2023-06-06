5ab0359f21a3f042621ea902d971944f59467073
Alexey Losev <reventon399@gmail.com>
2023-06-06 20:41:52 +0300

    Initial Commit

---
+++ PomodorroMVVM(Test_Task)/PomodorroMVVM_Test_Task_App.swift
@@ -0,0 +1,17 @@
+//
+//  PomodorroMVVM_Test_Task_App.swift
+//  PomodorroMVVM(Test_Task)
+//
+//  Created by Алексей Лосев on 06.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PomodorroMVVM_Test_Task_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
